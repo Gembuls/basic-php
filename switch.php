7b38e9b915948e75ad023961fd98234544ff3c35
agalicogati <84725719+Gembuls@users.noreply.github.com>
2021-11-21 22:29:28 +0700

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+$warnafav = "ungu";
+
+switch ($warnafav) {
+    case "blue":
+        echo "Warna Favorit Kamu Adalah biru";
+        break;
+    case "red":
+        echo "Warna Favorit Kamu Adalah Merah";
+    default:
+        echo "Warna Favorit Kamu Adalah Tidak Merah, Biru, Ataupun Hijau";
+        break;
+}
